96cb9b34d49260e988d255a382369652677826bb
Yashwanth Ravipati <yashwanth.ravipati@icloud.com>
2021-07-26 12:31:53 -0500

    Initial Commit

---
+++ Three Letter Word Hackwich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Three Letter Word Hackwich
+//
+//  Created by Yashwanth Ravipati on 7/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Three Letter Word Hackwich/Three_Letter_Word_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_Word_HackwichApp.swift
+//  Three Letter Word Hackwich
+//
+//  Created by Yashwanth Ravipati on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_Word_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

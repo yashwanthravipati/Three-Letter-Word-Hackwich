2ec214bebb077b0de66bb0a5a60266703d8ffd9a
Yashwanth Ravipati <yashwanth.ravipati@icloud.com>
2021-07-27 10:47:56 -0500

    Created tap gesture recognizer

--- Three Letter Word Hackwich/ContentView.swift
+++ Three Letter Word Hackwich/ContentView.swift
@@ -18,6 +18,15 @@ var body: some View {
             .fontWeight(.bold)
             .padding()
         Text("Tap the gray box to change the letter")
+        CustomLetterBox(color: Color.gray, text: letter)
+            .onTapGesture {
+                let position = alphabet.index(alphabet.startIndex, offsetBy: counter)
+                letter = String(alphabet[position])
+                counter += 1
+                if counter == alphabet.count {
+                    counter = 0
+                }
+            }
         }
     }
 }
